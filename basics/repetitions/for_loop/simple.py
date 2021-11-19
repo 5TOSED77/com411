b25545aba904303bc8950df857fc725560aabc8e
Donald Toseafa <5tosed77@solent.ac.uk>
2021-11-19 15:05:58 +0000

    How many mountains should I display?

---
+++ basics/repetitions/for_loop/simple.py
@@ -0,0 +1,12 @@
+mountains = input("How many mountains should I display?")
+print(mountains)
+
+for count in range(3):
+    print("           __  ")
+    print("          /  \_ ")
+    print("         /^    \ ")
+    print("        /  ^    \_ ")
+    print("      _/ ^ ^     ^\ ")
+    print("     /  ^     ^    \ ")
+
+print("Done!")
