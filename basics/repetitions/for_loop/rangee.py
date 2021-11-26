4b040fb6510c105974aca026f39ad999c9d8021c
Donald Toseafa <5tosed77@solent.ac.uk>
2021-11-26 15:30:31 +0000

    Added code to demonstrate the use of a simple for lo op.

---
+++ basics/repetitions/for_loop/rangee.py
@@ -0,0 +1,10 @@
+print("what level of brightness is required: ")
+brightness = int(input())
+
+for count in range(brightness, 10, 2):
+
+    print = int("beepsbrightnesslevel") + brightness
+
+    beepsbrightnesslevel = input()
+
+    bopsbrightnesslevel = input()
